df8ed2cf06569c5dfa1436135c564e085c3dd223
Dinesh <dineshwagh1996@gmail.com>
2021-08-04 10:03:12 +0530

    add my new projects files

---
+++ server/media/2021_07_28 21_25_44.py
@@ -0,0 +1,48 @@
+'''for i in range(0,10):
+ if i==4:
+  continue
+ print(i)
+ if i==6:
+  break'''
+'''i=0
+while(i<=5):
+ print("Dinu")
+ i=i+1
+a=["dinu","tushar","mohan","sagar"]
+for i in a:
+ print(i)
+i=2
+while(i<=10):
+ if i%2==0:
+  print(i)
+  i=i+2
+a=input("enter any number: ")
+a=int(a)
+if(a%2==0):
+ print("even number")
+else:
+ print("odd") 
+ 
+# using pass
+a=5
+b=5
+if a>b:
+ pass
+else:
+ pass
+ print("b is greater")
+ pass
+if(a==b):
+ print("equal")'''
+
+a=input("enter any number: ")
+b=input("enter any number: ")
+a=int(a)
+b=int(b)
+sq=a*b
+print("square is:",sq)
+
+ 
+ 
+
+
